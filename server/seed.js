f9f2daefe87d295a7e37758462f6fcda1f3465ba
Christian Richard Fairhall Smith <fairhallsmith@gmail.com>
2024-07-26 15:16:03 +0100

    Still not sure but I have some error catchers now thanks to Heinry.

--- server/seed.js
+++ server/seed.js
@@ -5,7 +5,7 @@ dotenv.config();
 
 const db = new pg.Pool({ connectionString: process.env."postgresql://postgres.quysmmwmiipxrjnzgbkv:bNqsj92Mn3ODkqEf@aws-0-eu-west-2.pooler.supabase.com:6543/postgres" });
 
-// create the books table
+// create the table
 db.query(`
 CREATE TABLE IF NOT EXISTS entries (
     id SERIAL PRIMARY KEY,
--- server/server.js
+++ server/server.js
@@ -12,13 +12,39 @@ app.get("/", function (req, res) {
   res.json("Radical simply means grasping things at the root.");
 });
 
-app.post("/entries", function (req, res) {
+app.post("/entries", async (req, res) => {
   // retrieve data from form
-  console.log(req.body);
+  const { username, message } = req.body;
+  if (!username || !message) {
+    return res
+      .status(400)
+      .json({ error: "Please enter your name and message." });
+  }
+  try {
+    const id = [entries]();
+    await db.run(
+      "INSERT INTO entries (id, username, message) VALUES (?, ?, ?)",
+      [id, username, message]
+    );
+    console.log("Message created - ", id);
+    res.status(201).json({ id, username, message });
+  } catch (error) {
+    console.error("Error sending messages:", error);
+    res.status(500).json({ error: "Failed to submit message." });
+  }
   // add new entry to database
-  res.json("Recieved it.");
+  res.json("Received it.");
 });
 
+app.get("/entries", async (req, res) => {
+  try {
+    const entries = await db.all("SELECT * FROM entries");
+    res.json(entries);
+  } catch (error) {
+    console.error("Error retrieving messages:", error);
+    res.status(500).json({ error: "Failed to retrieve messages." });
+  }
+});
 app.listen(8080, function () {
   console.log("Server is running on port 8080.");
 });
